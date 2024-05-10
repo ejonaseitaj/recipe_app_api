3fce80fa9522a3ee7b5b9182dbd73f0339787e27
Ejona Seitaj <ejonaseitaj@gmail.com>
2024-05-10 13:16:53 +0200

    adding admin customizations

--- app/core/models.py
+++ app/core/models.py
@@ -11,10 +11,24 @@ from django.db import models
 
 class UserManager(BaseUserManager):
     """Manager for users."""
+
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Email is required')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a superuser with the given"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
